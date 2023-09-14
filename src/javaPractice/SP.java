de4c19ce763f46c24ce763742e5f6a0021e389ac
tjoeun <tjoeun@DESKTOP-N876NQ2>
2023-09-14 17:51:25 +0900

    230914 이장춘 숙제

--- src/javaPractice/SP.java
+++ src/javaPractice/SP.java
@@ -1,15 +1,71 @@
 package javaPractice;
 
+import java.awt.Shape;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class SP {
 	static int num = 1;
 	public static void main(String[] args) {
-	
+		
+		Shape s1 = new Rectangle(10,3);
+		Shape s2 = new Circle(4);
+		
+		List<Shape> list = Arrays.asList(s1,s2);
+		
+		Map<Object, List<Shape>> map = list.stream().collect(Collectors.groupingBy(f -> f.getClass()));
+		map.get(Class.forName("sth")).stream().forEach(s -> System.out.println(s));
+		
+		
+		
+		try {
+			Path path = Paths.get("src");
+			Stream<Path> sr1 = Files.list(path);
+			sr1.forEach(s -> System.out.println(s.getFileName()));
+			
+			Stream<Path> sr2 = Files.find(path, 5, (p, BasicFileAttributes)->{
+				File file = p.toFile();
+				return !file.isDirectory() && file.getName().contains("Stream");
+			});
+			sr2.forEach(s -> System.out.println(s.getFileName()));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		try {
+			Path path = Paths.get("src/day20/strstream/strToStream.java");
+			Stream<String> sr =
+					Files.lines(path, Charset.forName("utf-8"));
+			
+			sr.forEach(s -> System.out.println(s));
+			sr.close();
+			System.out.println();
+			
+			File file = path.toFile();
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);//버퍼리더로 읽으면 스트림으로 들어간다.
+			br.lines().forEach(s -> System.out.println(s));;
+			br.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		String[] strArr = new String[] {"홍길동", "김유신", "이순신","유관순"};
 		
 		Stream<String> stream = Arrays.stream(strArr);
